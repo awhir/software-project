de456482fff8a58dbf46f7a60497be7143aec59b
awhir <whiriskey@gmail.com>
2014-02-18 18:25:43 +0000

    index

--- Software-Project/build/generated-sources/ap-source-output/entity/OrderedProduct_.java
+++ Software-Project/build/generated-sources/ap-source-output/entity/OrderedProduct_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2014-02-17T18:05:20")
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-02-18T16:35:55")
 @StaticMetamodel(OrderedProduct.class)
 public class OrderedProduct_ { 
 
